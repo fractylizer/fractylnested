80946185239df1ffe45abd3f82869246f4c690ed
Laura Trevisan <129926917+fractylizer@users.noreply.github.com>
2024-07-11 04:17:59 +1200

    Fixed activation check

--- fractylnested.js
+++ fractylnested.js
@@ -1,11 +1,11 @@
-var fractylNested = {
+if (typeof fractylNested == "undefined"){ var fractylNested = {
   load:function(){
     new Thing("fart",["nitrogen","oxygen","carbon","hydrogen","methane","sulfur"]);
     Things["village"].contains.push("cemetery,5%");
     Things["living-room"].contains.push("fart,5%");
     Things["wooden frame"].contains.push("nails,80%");
   }
-};
+};};
 console.log("FractylNested accessed.");
 if (typeof fractylNested.Activated == "undefined") {
   fractylNested.Activated = true;
