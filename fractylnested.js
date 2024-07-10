71d5747714ce00ad9b696fcb2435f34cd5826361
Laura Trevisan <129926917+fractylizer@users.noreply.github.com>
2024-07-11 02:32:11 +1200

    Added nails to wooden frames

--- fractylnested.js
+++ fractylnested.js
@@ -1,3 +1,4 @@
 new Thing("fart",["nitrogen","oxygen","carbon","hydrogen","methane","sulfur"]);
 Things["village"].contains.push("cemetery,5%");
 Things["living-room"].contains.push("fart,5%");
+Things["wooden frame"].contains.push("nails,80%");
