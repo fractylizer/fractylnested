c35485328e1bf59f66d0592cecc97b8f0d3f8e9d
Laura Trevisan <129926917+fractylizer@users.noreply.github.com>
2024-07-11 04:06:59 +1200

    Added activation check and logs

--- fractylnested.js
+++ fractylnested.js
@@ -1,4 +1,16 @@
-new Thing("fart",["nitrogen","oxygen","carbon","hydrogen","methane","sulfur"]);
-Things["village"].contains.push("cemetery,5%");
-Things["living-room"].contains.push("fart,5%");
-Things["wooden frame"].contains.push("nails,80%");
+var fractylNested = {}
+console.log("FractylNested accessed.")
+if (typeof fractylNested.Activated == undefined) {
+  fractylNested.Activated = true;
+  console.log("FractylNested activated.")
+  fractylNested.load();
+} else {
+  console.log("FractylNested already loaded.")
+}
+
+fractylNested.load = function() {
+  new Thing("fart",["nitrogen","oxygen","carbon","hydrogen","methane","sulfur"]);
+  Things["village"].contains.push("cemetery,5%");
+  Things["living-room"].contains.push("fart,5%");
+  Things["wooden frame"].contains.push("nails,80%");
+}
